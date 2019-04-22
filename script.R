e9fa906e8219ca4354e68be2c085484588e5e1f7
Shafi Rubbani <shafirubbani@gmail.com>
2019-04-22 17:06:14 -0400

    Editing data

--- script.R
+++ script.R
@@ -20,4 +20,9 @@ raw_springfield <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/0
 springfield <- raw_springfield %>% 
   mutate(latitude = as.numeric(gsub("[^0-9.-]", "", latitude))) %>%
   filter(!is.na(latitude)) %>% 
-  mutate(date = mdy(date))
+  mutate(date = mdy(date)) %>% 
+  
+  
+  
+  #mutate(time_range_1 = substr(time_range, 1, 6)
+           #substr(time_range, nchar(time_range) - 5, nchar(time_range)))
